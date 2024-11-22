b587f0aaf44fd5c9e837fcc495f0bb478ee3328c
DuongPAD <duongphananh@gmail.com>
2024-11-22 12:47:40 +0700

    update claim mantra node

--- claim-rewards.sh
+++ claim-rewards.sh
@@ -14,16 +14,18 @@ json_data=$(cat oliver.json)
 num_elements=$(echo "$json_data" | jq '. | length')
 
 inux_validator_address="cosmosvaloper1zgqal5almcs35eftsgtmls3ahakej6jmnn2wfj"
-# pryzm_validator_address="cosmosvaloper1hmd535f69t3x262m6s9wc6jd0dmel2zevhyuhm"
+mantra_validator_address="cosmosvaloper103agss48504gkk3la5xcg5kxplaf6ttnuv234h"
 
 # Loops through each element in the array
 for ((i=0; i<$num_elements; i++)); do
     username="oliver$(printf "%02d" $((i+1)))"
     printf "\e[34m$username\e[0m"
     echo
-    echo "y" | gaiad tx distribution withdraw-rewards "$inux_validator_address" --from="$username" --chain-id="cosmoshub-4" --node="https://cosmos-rpc.publicnode.com:443" --gas-adjustment 1.8 --gas auto --gas-prices 0.017uatom
+    echo "y" | gaiad tx distribution withdraw-rewards "$mantra_validator_address" --from="$username" --chain-id="cosmoshub-4" --node="https://cosmos-rpc.publicnode.com:443" --gas-adjustment 1.8 --gas auto --gas-prices 0.017uatom
     echo "====================================================="
     echo
-    sleep 0.5  # Sleep 0.5 second before continuing the loop
+    sleep 12 
+    echo "y" | gaiad tx distribution withdraw-rewards "$inux_validator_address" --from="$username" --chain-id="cosmoshub-4" --node="https://cosmos-rpc.publicnode.com:443" --gas-adjustment 1.8 --gas auto --gas-prices 0.017uatom
+    sleep 1
 done
 echo "Done! Claimed all rewards!"
