00a5ee6d4d698ee5881b39f25728c6b209f63bfa
DuongPAD <duongphananh@gmail.com>
2024-12-30 12:17:36 +0700

    check balance

--- send-elys.sh
+++ send-elys.sh
@@ -10,7 +10,7 @@ json_data=$(cat elys.json)
 num_elements=$(echo "$json_data" | jq '. | length')
 
 # Loop through each element in the array
-for ((i=0; i<num_elements; i++)); do
+for ((i=6; i<num_elements; i++)); do
   address=$(echo "$json_data" | jq -r ".[$i].address")
   username="oliver01"
 
@@ -20,10 +20,18 @@ for ((i=0; i<num_elements; i++)); do
   send_amount="50000ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"
   gas_prices="0.045ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"
 
-  echo "send money to $address"
-  echo "y" | elysd tx bank send oliver01 $address $send_amount --from="oliver01" --chain-id="elys-1" --node="https://rpc.elys.network:443" --gas-adjustment 1.8 --gas auto --gas-prices $gas_prices
-  sleep 16
-
+  balance_json=$(elysd query bank balances $address -o json --chain-id="elys-1" --node="https://rpc.elys.network:443")
+  balance=$(echo "$balance_json" | jq -r '.balances[] | select(.denom=="ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9") | .amount')
+  balance_in_millions=$(echo "scale=6; $balance / 1000000" | bc)
+  echo "Balance: $balance_in_millions"
+  if (( $(echo "$balance_in_millions < 0.05" | bc -l) )); then
+    echo "send money to $address"
+    echo "y" | elysd tx bank send oliver01 $address $send_amount --from="oliver01" --chain-id="elys-1" --node="https://rpc.elys.network:443" --gas-adjustment 1.8 --gas auto --gas-prices $gas_prices
+    sleep 14
+  else
+    echo "balance is greater than 0.05"
+    sleep 1
+  fi
 done
 echo
 echo "===================================================================================================="
